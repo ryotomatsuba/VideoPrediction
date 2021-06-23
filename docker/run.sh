96dd3d24cd712c8f654a8c3b5f240c4b110d2193
ryoto <metbyroot@gmail.com>
2021-06-23 11:14:16 +0900

    run=>excec

--- docker/run.sh
+++ docker/run.sh
@@ -7,9 +7,8 @@ docker run \
   -dit \
   --gpus all \
   -v $PWD:/workspace \
-  -v /home/data/ryoto:/home/data/ryoto \
+  -v /home/data/ryoto:/data \
   --name $CONTAINER_NAME \
   --rm \
   --shm-size=2g \
-  $IMAGE_NAME \
-  python3 train.py -cn mnist
+  $IMAGE_NAME
