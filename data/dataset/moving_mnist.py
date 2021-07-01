074f3c0d181c9ca32253557178bc8fe838602723
ryoto <metbyroot@gmail.com>
2021-07-01 23:29:21 +0900

    refactore mnist code

--- data/dataset/moving_mnist.py
+++ data/dataset/moving_mnist.py
@@ -5,9 +5,8 @@ sns.set()
 import cv2
 from math import *
 from scipy import signal
-import yaml
-import os
 import unittest
+from utils.draw import save_gif
 from omegaconf import DictConfig
 from torch.utils.data import Dataset
 class MnistDataset(Dataset):
@@ -15,8 +14,7 @@ class MnistDataset(Dataset):
     def __init__(self,cfg):
         mnist = np.load("/home/data/ryoto/Datasets/row/mnist.npz")
         num_data=cfg.dataset.num_data
-        index = np.arange(0, num_data, 1)
-        self.data = mv_mnist(num_data, index, mnist['X'].reshape(60000, 28, 28))
+        self.data = mv_mnist(num_data, mnist['X'].reshape(60000, 28, 28))
         self.input_num=cfg.dataset.input_num
 
     def __len__(self):
@@ -28,32 +26,29 @@ class MnistDataset(Dataset):
         return X
 
 
-def mv_mnist(num_sample, seeds, mnist):
-    data = np.zeros((num_sample, 10, 128, 128))
+def mv_mnist(num_sample, mnist):
+    _, mnist_h, mnist_w = mnist.shape
+    len_seq, output_h, output_w = 10, 128, 128 
+    seeds = np.arange(0, num_sample, 1)
+    data = np.zeros((num_sample, len_seq, output_h, output_w))
     for i in range(num_sample):
         np.random.seed(seeds[i])
-        da = np.zeros((10, 256, 256))
-        da2 = np.zeros((10, 256, 256))
-        x2 = np.random.randint(64, 192-28)
-        y2 = np.random.randint(64, 192-28)
-        x_start = np.random.randint(80, 160)
-        y_start = np.random.randint(80, 160)
-        x_0v = np.random.randint(-3, 3)
-        y_0v = np.random.randint(-3, 3)
-        x_a = int(np.random.normal()*0.1)
-        y_a = int(np.random.normal()*0.1)
+        da, da2 = np.zeros((2, len_seq, output_h*2, output_w*2)) # data for transition 
+        da3 = np.zeros((len_seq, output_h, output_w)) # data for rotation
+        x2, y2 = np.random.randint(64, 128+64-28, size=2)
+        x3, y3 = np.random.randint(0, 100, size=2)
+        x_start, y_start = np.random.randint(80, 160, size=2)
+        x_0v, y_0v = np.random.randint(-3, 3, size=2)
+        x_a, y_a = 0, 0 # no acceraration
         x, y = x_start, y_start
         ang_0 = np.random.randint(-6, 6)
         ang_a = int(np.random.normal()*0.1)     
         angles = 0
-        da3 = np.zeros((10, 128, 128))
-        x3 = np.random.randint(0, 100)
-        y3 = np.random.randint(0, 100)
         mask = np.random.rand(1).reshape(1, 1) 
         mask /= np.sum(mask) if np.sum(mask)!=0 else mask
         mask = (1 + np.random.uniform(-0.5, 0.5)) * mask
         da3[0, y3:y3+28, x3:x3+28] = mnist[seeds[i]+20000]
-        for t in range(10):
+        for t in range(len_seq):
             ang = ang_0 + ang_a*t
             angles += ang
             trans = cv2.getRotationMatrix2D((20, 20), angles, 1)
@@ -86,4 +81,15 @@ def mv_mnist(num_sample, seeds, mnist):
     return data      
 
 
-        
+class Test(unittest.TestCase):
+    def test(self):
+        cfg={"dataset":{"num_data":10,"input_num":4}}
+        cfg=DictConfig(cfg)
+        self.dataset=MnistDataset(cfg)
+        self.assertEqual(self.dataset[0].shape,(10,128,128))
+        self.assertEqual(len(self.dataset),cfg.dataset.num_data)
+    def tearDown(self) -> None:
+        save_gif(self.dataset[0],self.dataset[1])
+if __name__=="__main__":
+    unittest.main()
+    
