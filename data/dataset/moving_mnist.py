40ba75548c95024a379dd7e0c15293005008616b
ryoto <metbyroot@gmail.com>
2021-07-02 20:59:19 +0900

    rename

--- data/dataset/moving_mnist.py
+++ data/dataset/moving_mnist.py
@@ -34,11 +34,11 @@ def mv_mnist(num_sample, mnist):
     data = np.zeros((num_sample, len_seq, output_h, output_w))
     for i in range(num_sample):
         np.random.seed(seeds[i])
-        transition_mnist, rotation_mnist = np.zeros((2, len_seq, output_h*2, output_w*2)) # data for transition 
-        growth_decay_mnist = np.zeros((len_seq, output_h, output_w)) # data for rotation
-        x2, y2 = np.random.randint(64, 128+64-28, size=2)
-        x3, y3 = np.random.randint(0, 100, size=2)
-        x_start, y_start = np.random.randint(80, 160, size=2)
+        transition_mnist, rotation_mnist = np.zeros((2, len_seq, output_h*2, output_w*2)) # data for transition & rotation
+        growth_decay_mnist = np.zeros((len_seq, output_h, output_w)) # data for c
+        x_rot, y_rot = np.random.randint(64, 128+64-28, size=2) # rotate position
+        x_grow, y_grow = np.random.randint(0, 100, size=2) # growth/decay position
+        x_trans, y_trans = np.random.randint(80, 160, size=2) # trainsition position
         v0_x, v0_y = np.random.randint(-3, 3, size=2)
         a_x, a_y = 0, 0 # no acceraration
         ang_0 = np.random.randint(-6, 6)
@@ -46,21 +46,24 @@ def mv_mnist(num_sample, mnist):
         mask = np.random.rand(1).reshape(1, 1) 
         mask /= np.sum(mask) if np.sum(mask)!=0 else mask
         mask = (1 + np.random.uniform(-0.5, 0.5)) * mask
-        growth_decay_mnist[0, y3:y3+28, x3:x3+28] = mnist[seeds[i]+20000]
+        growth_decay_mnist[0, y_grow:y_grow+28, x_grow:x_grow+28] = mnist[seeds[i]+20000]
         for t in range(len_seq):
+            # rotation processing
             angles = ang_0*t + a_ang*t**2
-            trans = cv2.getRotationMatrix2D((20, 20), angles, 1)
+            rotation_matrix = cv2.getRotationMatrix2D((20, 20), angles, 1)
             mn = np.zeros((40, 40))
             mn[6:34, 6:34] = mnist[seeds[i]] # put mnist image on (40,40) canvas
-            rotation_mnist[t, x2:x2+40, y2:y2+40]  = cv2.warpAffine(mn, trans, (40, 40)) # rotate mnist image
-            x = x_start + v0_x*t + a_x*t**2
-            y = y_start + v0_y*t + a_y*t**2
+            rotation_mnist[t, x_rot:x_rot+40, y_rot:y_rot+40]  = cv2.warpAffine(mn, rotation_matrix, (40, 40)) # rotate mnist image
+            # trainsition processing
+            x = x_trans + v0_x*t + a_x*t**2
+            y = y_trans + v0_y*t + a_y*t**2
             try:
                 transition_mnist[t, x:x+28, y:y+28] = mnist[seeds[i]+30000] # put on mnist image
             except:
                 transition_mnist = np.zeros_like(transition_mnist)
-                print(i, t, x_start, y_start)
+                print(i, t, x_trans, y_trans)
                 break
+            # growth and decay processing
             if t>0:
                 da_t = signal.convolve2d(growth_decay_mnist[t-1], mask, mode = 'same')
             else:
@@ -84,8 +87,8 @@ class Test(unittest.TestCase):
         self.dataset=MnistDataset(cfg)
         self.assertEqual(self.dataset[0].shape,(10,128,128))
         self.assertEqual(len(self.dataset),cfg.dataset.num_data)
-    def tearDown(self) -> None:
         save_gif(self.dataset[0],self.dataset[1])
+    def tearDown(self) -> None:
 if __name__=="__main__":
     unittest.main()
     
