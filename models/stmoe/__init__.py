29ea397dbfafb208361cfa1753aeed4afec811c8
ryoto <metbyroot@gmail.com>
2021-08-13 19:54:06 +0900

    #9 add stmoe

---
+++ models/stmoe/__init__.py
@@ -0,0 +1,1 @@
+from .stmoe import STMoE
---
+++ models/stmoe/stmoe.py
@@ -0,0 +1,13 @@
+from torch import nn
+
+
+class STMoE(nn.Module):
+    """STMoE Network
+
+    """
+    def __init__(self):
+        pass
+    def forward(self, x):
+        pass
+    
+    
