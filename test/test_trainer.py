c1b6144927eac491dc82874625698d4c7a725086
ryoto <metbyroot@gmail.com>
2021-09-03 17:33:59 +0900

    #24 add unet test

--- test/test_trainer.py
+++ test/test_trainer.py
@@ -5,17 +5,18 @@ from trainers import UnetTrainer, PredRNNTrainer, STMoETrainer,get_trainer
 import os
 
 class TestTrainer(unittest.TestCase):
-    def setUp(self) -> None:
-        with initialize(config_path="../configs"):
-            # config is relative to a module
-            overrides=[
-                "train=development",
-                "model=unet",
-                "dataset=moving_mnist",
-                "dataset.num_data=4",
-                "experiment.name=unittest"]
-            self.cfg = compose(config_name="default", overrides=overrides)
+    def __init__(self, methodName: str) -> None:
 
+        # config is relative to a module
+        self.overrides=[
+            "train=development",
+            "dataset=moving_mnist",
+            "dataset.num_data=4",
+            "experiment.name=unittest"]
+        super().__init__(methodName=methodName)
+    
+    def setUp(self) -> None:
+        
         os.makedirs(".hydra", exist_ok=True)
         with open(".hydra/config.yaml","x") as f:
             f.write("")
@@ -23,11 +24,19 @@ class TestTrainer(unittest.TestCase):
             f.write("")
         return super().setUp()
     
-    def test_train(self)-> None:
+    def test_unet_trainer(self)-> None:
+        self.override_model("unet")
         trainer=get_trainer(self.cfg)
         trainer.train()
         self.assertTrue(os.path.exists("best_ckpt.pth"))
         self.assertTrue(os.path.exists("pred_train_0(0).gif"))
+    
+    def override_model(self, model_name: str) -> None:
+        with initialize(config_path="../configs"):
+            self.overrides.append(f"model={model_name}")
+            self.cfg = compose(config_name="default", overrides=self.overrides)
+        
+
 
     
 
