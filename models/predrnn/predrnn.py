ac08201a0f003249f0d1256d2dba1105e6bbc27c
ryoto <metbyroot@gmail.com>
2021-09-03 13:45:48 +0900

    #24 refactor predrnn

--- models/predrnn/predrnn.py
+++ models/predrnn/predrnn.py
@@ -2,38 +2,47 @@ __author__ = 'yunbo'
 
 import torch
 import torch.nn as nn
-from .SpatioTemporalLSTMCell import SpatioTemporalLSTMCell
+from models.predrnn.SpatioTemporalLSTMCell import SpatioTemporalLSTMCell
+from models.predrnn.utils import preprocess
+from torchsummary import summary
+import numpy as np
 
 
 class PredRNN(nn.Module):
-    def __init__(self,cfg):
+    def __init__(self,input_num=4,total_length=10,img_channel=1,img_width=128,patch_size=4,
+            num_hidden=[64,64,64,64],filter_size=5,stride=1,layer_norm=1):
         super(PredRNN, self).__init__()
-
-        self.cfg = cfg
-        self.frame_channel = cfg.model.patch_size * cfg.model.patch_size * cfg.dataset.img_channel
-        self.num_hidden = cfg.model.num_hidden
+        self.input_num = input_num
+        self.total_length = total_length
+        self.patch_size = patch_size
+        self.frame_channel = patch_size * patch_size * img_channel
+        self.num_hidden = num_hidden
+        self.reverse_scheduled_sampling=0
         self.num_layers = len(self.num_hidden)
         cell_list = []
-        if torch.cuda.is_available():
-            self.device = torch.device('cuda')
-        else:
-            self.device = torch.device('cpu')
 
 
-        width = cfg.dataset.img_width // cfg.model.patch_size
+        width = img_width // patch_size
         self.MSE_criterion = nn.MSELoss()
 
         for i in range(self.num_layers):
             in_channel = self.frame_channel if i == 0 else self.num_hidden[i - 1]
             cell_list.append(
-                SpatioTemporalLSTMCell(in_channel, self.num_hidden[i], width, cfg.model.filter_size,
-                                       cfg.model.stride, cfg.model.layer_norm)
+                SpatioTemporalLSTMCell(in_channel, self.num_hidden[i], width, filter_size,
+                                       stride, layer_norm)
             )
         self.cell_list = nn.ModuleList(cell_list)
         self.conv_last = nn.Conv2d(self.num_hidden[self.num_layers - 1], self.frame_channel,
                                    kernel_size=1, stride=1, padding=0, bias=False)
 
-    def forward(self, frames_tensor, mask_true):
+    def forward(self, frames_tensor, mask_true=None):
+        device=frames_tensor.device
+        frames_tensor=preprocess.reshape_patch(frames_tensor, self.patch_size)
+        frames_tensor = torch.tensor(frames_tensor, device=device, dtype=torch.float32)
+
+        if mask_true is None:
+            real_input_flag = np.ones(frames_tensor.shape)
+            mask_true = torch.tensor(real_input_flag,device=device,dtype=torch.float32)
         # [batch, length, height, width, channel] -> [batch, length, channel, height, width]
         frames = frames_tensor.permute(0, 1, 4, 2, 3).contiguous()
         mask_true = mask_true.permute(0, 1, 4, 2, 3).contiguous()
@@ -43,29 +52,30 @@ class PredRNN(nn.Module):
         width = frames.shape[4]
 
         next_frames = []
+
         h_t = []
         c_t = []
 
         for i in range(self.num_layers):
-            zeros = torch.zeros([batch, self.num_hidden[i], height, width]).to(self.device)
+            zeros = torch.zeros([batch, self.num_hidden[i], height, width]).to(device)
             h_t.append(zeros)
             c_t.append(zeros)
 
-        memory = torch.zeros([batch, self.num_hidden[0], height, width]).to(self.device)
+        memory = torch.zeros([batch, self.num_hidden[0], height, width]).to(device)
 
-        for t in range(self.cfg.dataset.num_frames - 1):
+        for t in range(self.total_length - 1):
             # reverse schedule sampling
-            if self.cfg.sampling.reverse_scheduled_sampling == 1:
+            if self.reverse_scheduled_sampling == 1:
                 if t == 0:
                     net = frames[:, t]
                 else:
                     net = mask_true[:, t - 1] * frames[:, t] + (1 - mask_true[:, t - 1]) * x_gen
             else:
-                if t < self.cfg.model.input_num:
+                if t < self.input_num:
                     net = frames[:, t]
                 else:
-                    net = mask_true[:, t - self.cfg.model.input_num] * frames[:, t] + \
-                          (1 - mask_true[:, t - self.cfg.model.input_num]) * x_gen
+                    net = mask_true[:, t - self.input_num] * frames[:, t] + \
+                          (1 - mask_true[:, t - self.input_num]) * x_gen
 
             h_t[0], c_t[0], memory = self.cell_list[0](net, h_t[0], c_t[0], memory)
 
@@ -77,8 +87,13 @@ class PredRNN(nn.Module):
 
         # [length, batch, channel, height, width] -> [batch, length, height, width, channel]
         next_frames = torch.stack(next_frames, dim=0).permute(1, 0, 3, 4, 2).contiguous()
-        loss = self.MSE_criterion(next_frames, frames_tensor[:, 1:])
-        return next_frames, loss
+        next_frames = preprocess.reshape_patch_back(next_frames, self.patch_size)
+
+        return next_frames, 0
 
 if __name__=="__main__":
-    pass
+    batch, total_length, h ,w= 1, 10, 128, 128
+    net=PredRNN()
+    summary(net,(total_length, h ,w, 1), device="cpu")
+    input = torch.rand(batch, total_length, h ,w)
+    output = net(input)
--- models/predrnn/utils/preprocess.py
+++ models/predrnn/utils/preprocess.py
@@ -1,22 +1,18 @@
 __author__ = 'yunbo'
 
 import numpy as np
-
+import torch
 def reshape_patch(img_tensor, patch_size):
     if img_tensor.ndim==4:
         img_tensor=img_tensor[...,np.newaxis]
     assert 5 == img_tensor.ndim
-    batch_size = np.shape(img_tensor)[0]
-    seq_length = np.shape(img_tensor)[1]
-    img_height = np.shape(img_tensor)[2]
-    img_width = np.shape(img_tensor)[3]
-    num_channels = np.shape(img_tensor)[4]
-    a = np.reshape(img_tensor, [batch_size, seq_length,
+    batch_size,seq_length,img_height,img_width,num_channels= img_tensor.shape
+    a = torch.reshape(img_tensor, [batch_size, seq_length,
                                 img_height//patch_size, patch_size,
                                 img_width//patch_size, patch_size,
                                 num_channels])
-    b = np.transpose(a, [0,1,2,4,3,5,6])
-    patch_tensor = np.reshape(b, [batch_size, seq_length,
+    a.permute(0,1,2,4,3,5,6)  # transpose 
+    patch_tensor = torch.reshape(a, [batch_size, seq_length,
                                   img_height//patch_size,
                                   img_width//patch_size,
                                   patch_size*patch_size*num_channels])
@@ -24,18 +20,14 @@ def reshape_patch(img_tensor, patch_size):
 
 def reshape_patch_back(patch_tensor, patch_size):
     assert 5 == patch_tensor.ndim
-    batch_size = np.shape(patch_tensor)[0]
-    seq_length = np.shape(patch_tensor)[1]
-    patch_height = np.shape(patch_tensor)[2]
-    patch_width = np.shape(patch_tensor)[3]
-    channels = np.shape(patch_tensor)[4]
+    batch_size,seq_length,patch_height,patch_width, channels= patch_tensor.shape
     img_channels = channels // (patch_size*patch_size)
-    a = np.reshape(patch_tensor, [batch_size, seq_length,
+    a = torch.reshape(patch_tensor, [batch_size, seq_length,
                                   patch_height, patch_width,
                                   patch_size, patch_size,
                                   img_channels])
-    b = np.transpose(a, [0,1,2,4,3,5,6])
-    img_tensor = np.reshape(b, [batch_size, seq_length,
+    a.permute(0,1,2,4,3,5,6)  # transpose 
+    img_tensor = torch.reshape(a, [batch_size, seq_length,
                                 patch_height * patch_size,
                                 patch_width * patch_size,
                                 img_channels])
