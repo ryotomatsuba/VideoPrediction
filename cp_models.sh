f72ab3490ffab0c511716381c685d0ce3ddbda09
ryoto <metbyroot@gmail.com>
2021-11-09 08:29:15 +0900

    add shell script

---
+++ cp_models.sh
@@ -0,0 +1,5 @@
+id=be74d1350cc04a0098bb5cc647148195
+path=/data/Result/mlruns/5/$id/artifacts/best_ckpt.pth
+name=sine_wave_high_x_mnist
+echo $path
+cp $path /data/Models/$name.pth
--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
-for model_name in unet_sine_wave_high.pth unet_sine_wave_low.pth unet_sine_wave_middle.pth unet_cifar10.pth
+for model_name in cifar10_x_sine_wave_low mnist_x_cifar10 sine_wave_high_x_sine_wave_low sine_wave_low_x_mnist sine_wave_high_x_cifar10 sine_wave_high_x_mnist
 do  
 echo $model_name
     # python3 train.py train=test dataset.frames_shift=100 dataset=video_data train.ckpt_path=/data/Models/$model_name
-    python3 train.py train=test dataset.num_data=1 dataset=moving_image train.ckpt_path=/data/Models/unet_mix.pth
+    python3 train.py train=test dataset.num_data=1 dataset=moving_image dataset.image_type=cifar10 train.ckpt_path=/data/Models/$model_name.pth
 done
 
 
